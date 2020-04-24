e5eb036e87e5f31c7e23f1123d21ad885072643e
aicha <ai.elabdellaoui@gmail.com>
2020-04-24 03:38:11 +0200

    hh

--- gestion-stage-v1/src/main/java/com/gestion/stage/bean/DocumentCommentaire.java
+++ gestion-stage-v1/src/main/java/com/gestion/stage/bean/DocumentCommentaire.java
@@ -5,6 +5,10 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -19,6 +23,7 @@ public class DocumentCommentaire implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	@Column(columnDefinition = "Text")
 	private String Contenu;
@@ -26,7 +31,9 @@ public class DocumentCommentaire implements Serializable{
 	private Date dateCreation;
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date dateModification;
+	@ManyToOne
 	private Utilisateur utilisateur;
+	@ManyToOne
 	private Document document;
 	
 }
