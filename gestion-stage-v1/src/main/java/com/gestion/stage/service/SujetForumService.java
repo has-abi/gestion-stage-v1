d9d69a5b26c16b4215a5c44117fb4a765d0f3e14
aicha <ai.elabdellaoui@gmail.com>
2020-05-03 03:11:01 +0200

    hh

--- gestion-stage-v1/src/main/java/com/gestion/stage/service/SujetForumService.java
+++ gestion-stage-v1/src/main/java/com/gestion/stage/service/SujetForumService.java
@@ -1,5 +1,15 @@
 package com.gestion.stage.service;
 
+import java.util.Date;
+import java.util.List;
+
+import com.gestion.stage.bean.SujetForum;
+
 public interface SujetForumService {
+	List<SujetForum> findByContent(String content);
+	List<SujetForum> findByDateCreation(Date dateCreation);
+	List<SujetForum> findByDateModification(Date dateModification);
+	int save(SujetForum sujetForum);
+	List<SujetForum> findAll();
 
 }
--- gestion-stage-v1/src/main/java/com/gestion/stage/service/TacheService.java
+++ gestion-stage-v1/src/main/java/com/gestion/stage/service/TacheService.java
@@ -1,5 +1,18 @@
 package com.gestion.stage.service;
 
+import java.util.Date;
+import java.util.List;
+
+import com.gestion.stage.bean.Tache;
+
 public interface TacheService {
+	List<Tache> findByContenu(String contenu);
+	List<Tache> findByDateCreation(Date dateCreation);
+	List<Tache> findByDateLimite(Date dateLimite);
+	int save(Tache tache);
+	int updateTache(Tache tache);
+	List<Tache> findAll();
+	
+	
 
 }
