ba303c5d1a79faa46f0d9081980673d2b536aace
abida hassan <jeanarnolde1995@gmail.com>
2020-05-04 16:11:07 +0000

    some changes

---
+++ gestion-stage-v1/src/main/java/com/gestion/stage/service/impl/PaysServiceImpl.java
@@ -0,0 +1,31 @@
+package com.gestion.stage.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.gestion.stage.bean.Pays;
+import com.gestion.stage.service.PaysService;
+
+@Service
+public class PaysServiceImpl implements PaysService{
+
+	@Override
+	public Pays findByNom(String nom) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int save(Pays pays) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Pays> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
